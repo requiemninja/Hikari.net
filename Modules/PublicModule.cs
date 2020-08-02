eefb785738ebdc418b7a6a1b8b62191b4128ab43
Samantha <2075754+requiemninja@users.noreply.github.com>
2020-08-02 17:19:20 -0400

    SFW Endpoints
    Added SFW endpoints and implemented them as commands

--- Modules/PublicModule.cs
+++ Modules/PublicModule.cs
@@ -4,6 +4,7 @@ using Discord;
 using Discord.Commands;
 using hikari.net.Services;
 using System.Linq;
+using System;
 
 namespace hikari.net.Modules
 {
@@ -17,25 +18,111 @@ namespace hikari.net.Modules
             => ReplyAsync("pong!");
 
         [Command("admin")]
-        public async Task adminPing()
+        public async Task AdminPing()
         {
             await Context.Channel.SendMessageAsync("Pinging <@&424793735151353869> for assistance!");
         }
 
-        [Command("nsfwneko")]
-        public async Task NSFWNekoAsync()
+        [Command("commands")]
+        public async Task CommandsList()
         {
-            var stream = await PictureService.GetNSFWNekoPictureAsync();
-            stream.Seek(0, SeekOrigin.Begin);
-            await Context.Channel.SendFileAsync(stream, "nsfwneko.png");
+            await Context.Channel.SendMessageAsync("Commands list placeholder");
         }
 
-        [Command("sfwneko")]
+        [Command("sfw cuddle")]
+        public async Task SFWCuddleAsync()
+        {
+            var stream = await PictureService.GetSFWCuddleAsync();
+            stream.Seek(0, SeekOrigin.Begin);
+            await Context.Channel.SendFileAsync(stream, "sfwcuddle.png");
+        }
+
+        [Command("sfw tickle")]
+        public async Task SFWTickleAsync()
+        {
+            var stream = await PictureService.GetSFWTickleAsync();
+            stream.Seek(0, SeekOrigin.Begin);
+            await Context.Channel.SendFileAsync(stream, "sfwtickle.png");
+        }
+
+        [Command("sfw slap")]
+        public async Task SFWSlapAsync()
+        {
+            var stream = await PictureService.GetSFWSlapAsync();
+            stream.Seek(0, SeekOrigin.Begin);
+            await Context.Channel.SendFileAsync(stream, "sfwslap.png");
+        }
+
+        [Command("sfw poke")]
+        public async Task SFWPokeAsync()
+        {
+            var stream = await PictureService.GetSFWPokeAsync();
+            stream.Seek(0, SeekOrigin.Begin);
+            await Context.Channel.SendFileAsync(stream, "sfwpoke.png");
+        }
+
+        [Command("sfw pat")]
+        public async Task SFWPatAsync()
+        {
+            var stream = await PictureService.GetSFWPatAsync();
+            stream.Seek(0, SeekOrigin.Begin);
+            await Context.Channel.SendFileAsync(stream, "sfwpat.png");
+        }
+
+        [Command("sfw neko")]
         public async Task SFWNekoAsync()
         {
-            var stream = await PictureService.GetSFWNekoPictureAsync();
+            var stream = await PictureService.GetSFWNekoAsync();
             stream.Seek(0, SeekOrigin.Begin);
             await Context.Channel.SendFileAsync(stream, "sfwneko.png");
+        }
+
+        [Command("sfw meow")]
+        public async Task SFWMeowAsync()
+        {
+            var stream = await PictureService.GetSFWMeowAsync();
+            stream.Seek(0, SeekOrigin.Begin);
+            await Context.Channel.SendFileAsync(stream, "sfwmeow.png");
+        }
+
+        [Command("sfw lizard")]
+        public async Task SFWLizardAsync()
+        {
+            var stream = await PictureService.GetSFWLizardAsync();
+            stream.Seek(0, SeekOrigin.Begin);
+            await Context.Channel.SendFileAsync(stream, "sfwlizard.png");
+        }
+
+        [Command("sfw kiss")]
+        public async Task SFWKissAsync()
+        {
+            var stream = await PictureService.GetSFWKissAsync();
+            stream.Seek(0, SeekOrigin.Begin);
+            await Context.Channel.SendFileAsync(stream, "sfwkiss.png");
+        }
+
+        [Command("sfw hug")]
+        public async Task SFWHugAsync()
+        {
+            var stream = await PictureService.GetSFWHugAsync();
+            stream.Seek(0, SeekOrigin.Begin);
+            await Context.Channel.SendFileAsync(stream, "sfwhug.png");
+        }
+
+        [Command("sfw foxgirl")]
+        public async Task SFWFoxGirlAsync()
+        {
+            var stream = await PictureService.GetSFWFoxGirlAsync();
+            stream.Seek(0, SeekOrigin.Begin);
+            await Context.Channel.SendFileAsync(stream, "sfwfoxgirl.png");
+        }
+
+        [Command("sfw feed")]
+        public async Task SFWFeedAsync()
+        {
+            var stream = await PictureService.GetSFWFeedAsync();
+            stream.Seek(0, SeekOrigin.Begin);
+            await Context.Channel.SendFileAsync(stream, "sfwfeed.png");
         }
 
         [Command("ban")]
@@ -45,7 +132,8 @@ namespace hikari.net.Modules
         public async Task BanUserAsync(IGuildUser user, [Remainder] string reason = null)
         {
             await user.Guild.AddBanAsync(user, reason: reason);
-            await ReplyAsync("ok!");
+            await ReplyAsync($"{user} has been banned for {reason}.");
+            //await ReplyAsync(user + " has been banned for " + reason + );
         }
     }
 }
--- Services/PictureService.cs
+++ Services/PictureService.cs
@@ -13,19 +13,91 @@ namespace hikari.net.Services
         public PictureService(HttpClient http)
             => _http = http;
 
-        public async Task<Stream> GetSFWNekoPictureAsync()
+        public async Task<Stream> GetSFWCuddleAsync()
         {
-            NekosImage image = await NekosClient.GetRandomSfwAsync();
+            NekosImage image = await NekosClient.GetSfwAsync(Nekos.Net.Endpoints.SfwEndpoint.Cuddle);
             var resp = await _http.GetAsync(image.FileUrl);
             return await resp.Content.ReadAsStreamAsync();
         }
 
-        public async Task<Stream> GetNSFWNekoPictureAsync()
+        public async Task<Stream> GetSFWTickleAsync()
         {
-            NekosImage image = await NekosClient.GetRandomNsfwAsync();
+            NekosImage image = await NekosClient.GetSfwAsync(Nekos.Net.Endpoints.SfwEndpoint.Tickle);
             var resp = await _http.GetAsync(image.FileUrl);
             return await resp.Content.ReadAsStreamAsync();
         }
+
+        public async Task<Stream> GetSFWSlapAsync()
+        {
+            NekosImage image = await NekosClient.GetSfwAsync(Nekos.Net.Endpoints.SfwEndpoint.Slap);
+            var resp = await _http.GetAsync(image.FileUrl);
+            return await resp.Content.ReadAsStreamAsync();
+        }
+
+        public async Task<Stream> GetSFWPokeAsync()
+        {
+            NekosImage image = await NekosClient.GetSfwAsync(Nekos.Net.Endpoints.SfwEndpoint.Poke);
+            var resp = await _http.GetAsync(image.FileUrl);
+            return await resp.Content.ReadAsStreamAsync();
+        }
+
+        public async Task<Stream> GetSFWPatAsync()
+        {
+            NekosImage image = await NekosClient.GetSfwAsync(Nekos.Net.Endpoints.SfwEndpoint.Pat);
+            var resp = await _http.GetAsync(image.FileUrl);
+            return await resp.Content.ReadAsStreamAsync();
+        }
+
+        public async Task<Stream> GetSFWNekoAsync()
+        {
+            NekosImage image = await NekosClient.GetSfwAsync(Nekos.Net.Endpoints.SfwEndpoint.Neko);
+            var resp = await _http.GetAsync(image.FileUrl);
+            return await resp.Content.ReadAsStreamAsync();
+        }
+
+        public async Task<Stream> GetSFWMeowAsync()
+        {
+            NekosImage image = await NekosClient.GetSfwAsync(Nekos.Net.Endpoints.SfwEndpoint.Meow);
+            var resp = await _http.GetAsync(image.FileUrl);
+            return await resp.Content.ReadAsStreamAsync();
+        }
+
+        public async Task<Stream> GetSFWLizardAsync()
+        {
+            NekosImage image = await NekosClient.GetSfwAsync(Nekos.Net.Endpoints.SfwEndpoint.Lizard);
+            var resp = await _http.GetAsync(image.FileUrl);
+            return await resp.Content.ReadAsStreamAsync();
+        }
+
+        public async Task<Stream> GetSFWKissAsync()
+        {
+            NekosImage image = await NekosClient.GetSfwAsync(Nekos.Net.Endpoints.SfwEndpoint.Kiss);
+            var resp = await _http.GetAsync(image.FileUrl);
+            return await resp.Content.ReadAsStreamAsync();
+        }
+
+        public async Task<Stream> GetSFWHugAsync()
+        {
+            NekosImage image = await NekosClient.GetSfwAsync(Nekos.Net.Endpoints.SfwEndpoint.Hug);
+            var resp = await _http.GetAsync(image.FileUrl);
+            return await resp.Content.ReadAsStreamAsync();
+        }
+
+        public async Task<Stream> GetSFWFoxGirlAsync()
+        {
+            NekosImage image = await NekosClient.GetSfwAsync(Nekos.Net.Endpoints.SfwEndpoint.Fox_Girl);
+            var resp = await _http.GetAsync(image.FileUrl);
+            return await resp.Content.ReadAsStreamAsync();
+        }
+
+        public async Task<Stream> GetSFWFeedAsync()
+        {
+            NekosImage image = await NekosClient.GetSfwAsync(Nekos.Net.Endpoints.SfwEndpoint.Feed);
+            var resp = await _http.GetAsync(image.FileUrl);
+            return await resp.Content.ReadAsStreamAsync();
+        }
+
+
         //NekosImage image = await NekosClient.GetSfwAsync(Nekos.Net.Endpoints.SfwEndpoint.Cuddle);
     }
 }
